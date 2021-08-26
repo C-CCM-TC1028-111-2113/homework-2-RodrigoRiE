950c39f4907bf0028244a8573bcb1b953802040a
RodrigoRiE <88850926+RodrigoRiE@users.noreply.github.com>
2021-08-25 23:26:29 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     # Escribe el código adecuado para completar el programa
+    A=float(input("Ingrese un numero: "))
+    B=float(input("Ingrese otro numero: "))
+    C=float(input("Ingrese un nuemero: "))
+    
+    if (C>B and B>A):
+        print(A," , ",B," , ",C)
+    elif (C>A and A>B):
+        print(B," , ",A," , ",C)
+    elif (B>A and A>C):
+        print(C," , ",A," , ",B)
+    elif (A>B and B>C):
+        print(C," , ",B," , ",A)
+    elif (B>C and C>A):
+        print(A," , ",C," , ",B)
+    elif (A>C and C>B):
+        print(B," , ",C," , ",A)
+    else:
+        print("Son numeros iguales")
     pass
 
 
