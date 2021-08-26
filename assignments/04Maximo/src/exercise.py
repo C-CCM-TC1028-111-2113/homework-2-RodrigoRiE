96d81717017c92f9a6afef820ec4c0790ce3b295
RodrigoRiE <88850926+RodrigoRiE@users.noreply.github.com>
2021-08-25 22:56:05 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    A=float(input("Ingrese un numero: "))
+    B=float(input("Ingrese otro numero: "))
+    C=float(input("Ingrese un nuemero: "))
+    if(A > B and A > C):
+        print("El numero mayor es ",A)
+    else:
+        if(B > A and B > C):
+            print("El numero mayor es ", B)
+        else:
+            print("El numero mayor es " , C)
+
     pass
 
 
